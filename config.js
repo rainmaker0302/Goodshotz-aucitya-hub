89cf30b457cd7716b45d808345415866d10d6dc7
rainmaker0302 <53500995+rainmaker0302@users.noreply.github.com>
2026-05-20 16:19:35 +0530

    Connect Supabase sync and reset starter data

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.GS_PROJECT_SYNC = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://gapybkbbmmncnqwyojwl.supabase.co",
+  supabaseAnonKey: "sb_publishable_PgIL0JRBu2pt_R3ev03L1w_7OnydI-E",
   roomId: "goodshotz-aucitya-home"
 };
